e1db20052fbe781a6ef217d8813c42b8c5e692fe
01081422 <90700682+01081422peru@users.noreply.github.com>
2021-11-10 21:37:20 +0200

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <time.h>
+using namespace std;
+void Create(int* a, const int size, const int Low, const int High, int i)
+{
+	a[i] = Low + rand() % (High - Low + 1);
+	if (i < size - 1)
+		Create(a, size, Low, High, i + 1);
+}
+void Print(int* a, const int size, int i)
+{
+	cout << setw(4) << a[i];
+	if (i < size - 1)
+		Print(a, size, i + 1);
+	else
+		cout << endl;
+}
+int Min(int a[], const int size, int i, int min)
+{
+	if (min > a[i] && a[i] % 2 == 0)
+		min = a[i];
+	if (i < size - 1)
+		return Min(a, size, i + 1, min);
+	else
+		return min;
+}
+int main()
+{
+	srand((unsigned)time(NULL)); // ініціалізація генератора випадкових чисел
+	 int n ;
+	 cout << "n = "; cin >> n;
+
+	 int* a = new int[n];
+	int Low = -10;
+	int High = 10;
+	Create(a, n, Low, High, 0);
+	Print(a, n, 0);
+	cout << "min = " << Min(a, n, 1, a[0]) << endl;
+
+	delete[] a;
+	return 0;
+}
